ed907255da5bca366f1f29cb8b1b7b85ceb2e893
alustau <denisalustau@gmail.com>
2017-06-20 23:30:26 -0300

    Adicionando as migrations de médicos e consulta

---
+++ database/migrations/2017_06_21_022111_create_doctor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('specialty');
+            $table->string('registry')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor');
+    }
+}
---
+++ database/migrations/2017_06_21_022527_create_appointment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('doctor_id');
+            $table->string('patient_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
